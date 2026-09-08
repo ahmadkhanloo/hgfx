4f432dec6712ffcacf64a4df270fbac7bc0eaa9b
Mohammad Ahmadkhanloo <30322781+ahmadkhanloo@users.noreply.github.com>
2026-09-08 11:56:12 +0330

    feat(M12H): export complete perceptual model surface

--- src/hgfx/models/__init__.py
+++ src/hgfx/models/__init__.py
@@ -5,14 +5,32 @@ from .hgf_binary import hgf_binary
 from .uhgf import uhgf
 from .uhgf_binary import uhgf_binary
 from .hgf_binary_pu import (
-    hgf_binary_pu,
-    ehgf_binary_pu,
-    uhgf_binary_pu,
-    hgf_binary_pu_tbt,
-    ehgf_binary_pu_tbt,
-    uhgf_binary_pu_tbt,
+    hgf_binary_pu, ehgf_binary_pu, uhgf_binary_pu,
+    hgf_binary_pu_tbt, ehgf_binary_pu_tbt, uhgf_binary_pu_tbt,
 )
 from .hgf_ar1_binary import hgf_ar1_binary, ehgf_ar1_binary, uhgf_ar1_binary
+from .hgf_ar1 import hgf_ar1
+from .mab import (
+    hgf_binary_mab,
+    hgf_ar1_mab,
+    hgf_ar1_binary_mab,
+    ehgf_ar1_binary_mab,
+    uhgf_ar1_binary_mab,
+)
+from .jget import hgf_jget, ehgf_jget, uhgf_jget
+from .categorical_world import (
+    hgf_categorical,
+    hgf_categorical_norm,
+    hgf_whatworld,
+    hgf_whichworld,
+)
+from .hhmm import (
+    HHMMNode,
+    hhmm_default_config_tree,
+    hhmm_prior_vectors,
+    hhmm_transform,
+    hierarchical_hidden_markov_model,
+)
 from .legacy import (
     rw_binary,
     rw_binary_dual,
@@ -23,28 +41,16 @@ from .legacy import (
 )
 
 __all__ = [
-    "hgf",
-    "hgf_binary",
-    "ehgf",
-    "ehgf_binary",
-    "uhgf",
-    "uhgf_binary",
-    "hgf_binary_pu",
-    "ehgf_binary_pu",
-    "uhgf_binary_pu",
-    "hgf_binary_pu_tbt",
-    "ehgf_binary_pu_tbt",
-    "uhgf_binary_pu_tbt",
-    "hgf_ar1_binary",
-    "ehgf_ar1_binary",
-    "uhgf_ar1_binary",
-    "rw_binary",
-    "rw_binary_dual",
-    "pearce_hall_binary",
-    "sutton_k1_binary",
-    "kalman_filter",
-    "hidden_markov_model",
-    "hgf_ar1",
+    "hgf", "hgf_binary", "ehgf", "ehgf_binary", "uhgf", "uhgf_binary",
+    "hgf_binary_pu", "ehgf_binary_pu", "uhgf_binary_pu",
+    "hgf_binary_pu_tbt", "ehgf_binary_pu_tbt", "uhgf_binary_pu_tbt",
+    "hgf_ar1_binary", "ehgf_ar1_binary", "uhgf_ar1_binary", "hgf_ar1",
+    "hgf_binary_mab", "hgf_ar1_mab", "hgf_ar1_binary_mab",
+    "ehgf_ar1_binary_mab", "uhgf_ar1_binary_mab",
+    "hgf_jget", "ehgf_jget", "uhgf_jget",
+    "hgf_categorical", "hgf_categorical_norm", "hgf_whatworld", "hgf_whichworld",
+    "HHMMNode", "hhmm_default_config_tree", "hhmm_prior_vectors", "hhmm_transform",
+    "hierarchical_hidden_markov_model",
+    "rw_binary", "rw_binary_dual", "pearce_hall_binary", "sutton_k1_binary",
+    "kalman_filter", "hidden_markov_model",
 ]
-
-from .hgf_ar1 import hgf_ar1
